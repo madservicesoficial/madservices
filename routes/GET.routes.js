b99231079b9cf7350980197fd6f3c8007f8807b7
Alejandro Fernández Pérez <alex.audiovisual.engineer@gmail.com>
2023-04-16 20:19:00 +0200

    37º commit

--- routes/GET.routes.js
+++ routes/GET.routes.js
@@ -2,6 +2,12 @@
 var servidor = require('express');
 //-- Importamos el Componente de Express que enrruta las paginas de MAD Services.
 var rutasGet = servidor.Router();
+//-- Importamos la función que comprueba que no se repita el ID aleatorio.
+const comprobarIDclientedb = require('../comprobarIDs/comprobarIDcliente.js');
+//-- Importamos la función que solicita los datos del cliente para tenerlos en variables.
+const comprobarDatosclientedb = require('../comprobarDatos/comprobarDatosCliente.js');
+//-- Importamos la función que solicita los datos de la empresa para tenerlos en variables.
+const comprobarDatosempresadb = require('../comprobarDatos/comprobarDatosEmpresa.js');
 
 //-- Ruta al Inicio de MAD Services.
 rutasGet.get('/', (req, res) => {
@@ -102,6 +108,36 @@ rutasGet.get('/sesion/:id/categorias', (req,res) => {
   let id = req.params.id;
   res.render('paginas/categoriasAuth', {id: id});
   return res.end();
+});
+
+//-- Ruta a la Sección del Perfil de los Clientes o de las Empresas de MAD Services.
+rutasGet.get('/sesion/:id/perfil', (req,res) => {
+  let id = req.params.id;
+  comprobarIDclientedb
+  (
+    id,
+    (existenciaID) => {
+      if(existenciaID) {
+        comprobarDatosclientedb
+        (
+          id,
+          (datos) => {
+            res.render('paginas/perfilClientes', {id: id, nombre: datos.nombre, apellidos: datos.apellidos});
+            return res.end();
+          }
+        );
+      }else {
+        comprobarDatosempresadb
+        (
+          id,
+          (datos) => {
+            res.render('paginas/perfilEmpresas', {id: id, nombre: datos.nombre, apellidos: datos.apellidos});
+            return res.end();
+          }
+        );
+      }
+    }
+  );
 });
 
 //-- Cerrar Sesión como Cliente o Empresa.
