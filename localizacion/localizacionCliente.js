9e79204eae0b3aaf9e68c5a0550a6452fab8578e
Alejandro Fernández Pérez <alex.audiovisual.engineer@gmail.com>
2023-05-06 18:25:38 +0200

    102º commit

--- localizacion/localizacionCliente.js
+++ localizacion/localizacionCliente.js
@@ -1,16 +1,16 @@
 //-- Importamos las funciones de operaciones de los Clientes para interactuar con la base de datos.
 const { registrarClienteVerificadodb } = require('../modelos/clientes/operacionesDB.js');
 //-- Importamos la Tecnología para validar el país introducido.
-const {getCountries, getCode} = require('country-list-spanish');
+const {getCountries, getCode } = require('country-list-spanish');
 //-- Importamos la Tecnología para validar el Código Postal introducido.
 const { postcodeValidator } = require('postcode-validator');
-//-- Importamos la Tecnología que verifica la localización del Cliente con su ubicación en tiempo real.
-const GeoNames = require('node-geonames-client');
 //-- Importamos la configuración del entorno ENV para poder usar su información.
 require('../config/env.js');
+//-- Importamos la Tecnología para solicitar URLs de Geolocalización.
+const axios = require('axios');
 
 //-- Creamos la función para verificar la localización del cliente.
-function localizacionCliente(data, res) {
+const localizacionCliente = async (data, res) => {
 
     //-- Obtenemos los campos de data de los Clientes.
     const email = data.email; 
@@ -27,40 +27,45 @@ function localizacionCliente(data, res) {
     const paises = getCountries();
     //-- Proceso de verificación de la localización.
     if(paises.includes(pais)) {
-        //-- Declaración de la cte que saca el código del país en español.
         const codigoPais = getCode(pais);
-        
-            /* if(existeRegion > 0) {
-                //-- Localizamos la población.
-                geonames.search({ country: codigoPais, maxRows: 1, featureClass: 'P' })
-                .then((resultado) => {
-                    //-- Declaramos la cte de la existencia de la población.
-                    const existePoblacion = resultado[0];
-                    if(existePoblacion > 0) {
-                        //-- Localizamos la dirección.
-                        geonames.search({ country: codigoPais, maxRows: 1, featureClass: 'S' })
-                        .then((results) => {
-                            //-- Declaramos la cte de la existencia de la dirección.
-                            const existeDireccion = results[0];
-                            if(existeDireccion > 0) {
-                                //-- Registramos el Cliente en la base de datos de MAD Services, verificando que no existía ya.
-                                registrarClienteVerificadodb
-                                (
-                                    {email: email, password: password, nombre: nombre, apellidos: apellidos, direccion: direccion,
-                                    poblacion: poblacion, region: region, pais: pais, cp: cp, genero: genero},
-                                    res
-                                );
-                            }
-                        });
+        if(postcodeValidator(cp, codigoPais)) {
+            //-- Enviamos una solicitud HTTP a la API de Geonames.
+            const response = await axios.get('http://api.geonames.org/postalCodeLookupJSON', {
+                params: {
+                    postalcode: cp,
+                    country: codigoPais,
+                    username: process.env.USUARIO_DE_GEONAMES
+                }
+            });
+            const lugar = response.data.postalcodes[0];
+            if(region === lugar.adminName1) {
+                if(poblacion === lugar.adminName3) {
+                    const minLong = 5;
+                    const maxLong = 48;
+                    if(direccion.length >= minLong && direccion.length <= maxLong) {
+                        //-- Registramos el Cliente en la base de datos de MAD Services, verificando que no existía ya.
+                        registrarClienteVerificadodb
+                        (
+                            {email: email, password: password, nombre: nombre, apellidos: apellidos, direccion: direccion,
+                            poblacion: poblacion, region: region, pais: pais, cp: cp, genero: genero},
+                            res
+                        );
                     }else {
-                        res.status(401).render('paginas/clientes/registrarse', {mensaje: 'Población incorrecta'});
+                        res.status(401).render('paginas/clientes/registrarse', {mensaje: `Dirección de ${poblacion} incorrecta`});
                         return res.end();
                     }
-                });
+                }else {
+                    res.status(401).render('paginas/clientes/registrarse', {mensaje: `Población de ${region} incorrecta`});
+                    return res.end();
+                }
             }else {
-                res.status(401).render('paginas/clientes/registrarse', {mensaje: 'Región incorrecta'});
+                res.status(401).render('paginas/clientes/registrarse', {mensaje: `Región de ${pais} incorrecta`});
                 return res.end();
-            } */
+            }
+        }else {
+            res.status(401).render('paginas/clientes/registrarse', {mensaje: 'Código Postal incorrecto'});
+            return res.end();
+        }
     }else {
         res.status(401).render('paginas/clientes/registrarse', {mensaje: 'País incorrecto'});
         return res.end();
