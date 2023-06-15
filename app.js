746f3d9d8a0729f6a154c5d9c22051dca1f74ec1
Alejandro Fernández Pérez <alex.audiovisual.engineer@gmail.com>
2023-06-16 00:45:40 +0200

    161º commit

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-//-- E-COMMERCE DE LOS E-COMMERCES: MAD Services =>
-//-- ########################################################################################## --//
+//--##########################################################################################--//
+//--####################### E-COMMERCE DE LOS E-COMMERCES: MAD Services => ###################--//
+//--##########################################################################################--//
 
 //##############################################################################################################//
 //-- DEPENDENCIAS, TECNOLOGÍAS E IMPORTACIONES USADAS =>
 //-- Importamos la Tecnología HTTP Errors para indicar errores internos que el E-Commerce tiene en el código,
 //-- y, así, el servidor no pueda generar código HTML para devolver al usuario.
 var controladorErrores = require('http-errors');
+//-- Importamos la configuración del entorno ENV para poder usar su información.
+require('./config/env.js');
 //-- Importamos la Tecnología Express para crear el servidor de MAD Services basado en Express.
 var servidor = require('express');
 //-- Importamos el Componente de la Tecnología Express para que los Miembros de MAD Services se puedan autenticar.
@@ -14,20 +17,13 @@ var session = require('express-session');
 const patchdeletemethods = require('method-override');
 //-- Importamos la Tecnología que enrruta todas las rutas de MAD Services.
 var path = require('path');
-//-- Importamos la Tecnología basada en un middleware que analiza el encabezado Cookie y, rellena req.cookies 
-//-- con objeto marcado con los nombres de las cookies.
 var analizadorCookies = require('cookie-parser');
 //-- Importamos la Tecnología basada, exclusivamente, en servir el favicon implícito predeterminado.
 //-- Para iconos adicionales específicos del proveedor que requieren Marcado HTML, se requiere middleware adicional para servir los archivos relevantes.
 var analizadorFavicon = require('serve-favicon');
-//-- Importamos la Tecnología basada en un middleware que analiza los cuerpos de solicitud entrantes,
-//-- antes de sus controladores y, está disponible bajo el objeto req.body.
 var analizadorBody = require('body-parser');
 //-- Importamos la Tecnología de Protección de Cabeceras HTTP.
 var protectorCabeceras = require('helmet');
-//-- Importamos la Tecnología de Control de Acceso HTTP que intercambia Recursos de Origen Cruzado (Crossed Origin ResourceS - CORS),
-//-- para utilizar cabeceras HTTP adicionales y, para permitir que un agente de usuario obtenga permiso para acceder a recursos
-//-- seleccionados desde un servidor en un origen distinto al que pertenece.
 var controlAccesoHTTP = require('cors');
 //-- Importamos las rutas de MAD Services.
 var rutasGet = require('./routes/GET.routes.js');
@@ -44,7 +40,7 @@ var madservices = servidor();
 //##############################################################################################################//
 //-- SESIÓN EN MAD SERVICES =>
 madservices.use(session({
-  secret: 'madserviceskey',
+  secret: process.env.COOKIE_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: { secure: true }
@@ -55,10 +51,7 @@ madservices.use(session({
 //-- MIDDLEWARES O SOFTWARES INTERMEDIOS => Para....                                                                          
 //-- 1. Trabajar con los ficheros JSON de la conexión.                                                                      
 //-- 2. Analizar los cuerpos codificados en URL que sólo examinan las solicitudes HTTP donde el encabezado Content-Type 
-//-- coincida con la opción de tipo (para el Body y para la Tecnología Express en general).                    
-//-- 3. Trabajar con las Cookies que se encuentran en la conexión.   
-//-- 4. Analizar el Body en formato JSON.
-//-- 5. Analizar el Body y su URL codificada.                                                        
+//-- coincida con la opción de tipo (para el Body y para la Tecnología Express en general).                                                      
 //-- 6. Analizar el favicon servido implícito predeterminado de MAD Services por parte del servidor.
 //-- 7. Analizar las cabeceras HTTP de cada conexión.
 //-- 8. Analizar el Control de Acceso HTTP de cada conexión.
@@ -67,18 +60,15 @@ madservices.use(session({
 //-- 11. Poder usar los métodos PATCH y DELETE.                                                                                                                 
 madservices.use(servidor.json());
 madservices.use(servidor.urlencoded({ extended: true }));
-madservices.use(analizadorCookies('madserviceskey'));
+madservices.use(analizadorCookies(process.env.COOKIE_SECRET));
 madservices.use(analizadorBody.json());
 madservices.use(analizadorBody.urlencoded({ extended: true }));
 madservices.use(analizadorFavicon(path.join(__dirname, 'public', 'favicon.ico')));
 madservices.use((req, res, next) => {
-
   //-- Dominio que tengan acceso.
   res.setHeader('Access-Control-Allow-Origin', '*');
-
   //-- Metodos de solicitud que deseas permitir.
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
   //-- Encabecedados que permites.
   res.setHeader('Access-Control-Request-Headers', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
